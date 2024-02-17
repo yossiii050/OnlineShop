a1e5870151bc3bc5f83a0c1bdcf04cb1aff0e78f
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2024-02-17 22:41:23 +0200

    hash passwords

--- OnlineShop/Models/User.cs
+++ OnlineShop/Models/User.cs
@@ -8,8 +8,10 @@ namespace OnlineShop.Models
         public string Id { get; set; }
         [Required]
         public string Name { get; set; }
+        
         [EmailAddress]
         public string Email { get; set; }
+        //need add hash passwords
         [Required]
         public string Password { get; set; }
 
