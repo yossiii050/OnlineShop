1acbe0d237103977c06e416b7dc5b11342ebe2ff
neriala <neria616@gmail.com>
2024-03-05 14:10:44 +0200

    newDashboard

--- OnlineShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlineShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
@@ -19,6 +20,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using OnlineShop.Models;
+using System.Data.SqlClient
+
 
 namespace OnlineShop.Areas.Identity.Pages.Account
 {
@@ -117,7 +120,97 @@ namespace OnlineShop.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = CreateUser();
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.CreditCardNumber = "";
+
+                var addToRoleResult = await _userManager.AddToRoleAsync(user, "Admin");
+                if (addToRoleResult.Succeeded)
+                {
+                    await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                    await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                    var result = await _userManager.CreateAsync(user, Input.Password);
+
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User created a new account with password.");
+
+                        var userId = await _userManager.GetUserIdAsync(user);
+
+                        // Add the new admin user to the admins table if they have been successfully assigned the "Admin" role
+                        if (await _userManager.IsInRoleAsync(user, "Admin"))
+                        {
+                            // Connect to your database here
+                            // Insert the new admin record
+                            using (var connection = new SqlConnection("conn"))
+                            {
+                                var command = new SqlCommand(
+                                    "INSERT INTO [dbo].[admins] (Name, Username, Password, Email, IsActive) VALUES (@Name, @Username, @Password, @Email, @IsActive)",
+                                    connection);
+                                command.Parameters.AddWithValue("@Name", user.FirstName + " " + user.LastName);
+                                command.Parameters.AddWithValue("@Username", user.UserName);
+                                command.Parameters.AddWithValue("@Password", Input.Password); // Consider not storing plain-text passwords
+                                command.Parameters.AddWithValue("@Email", user.Email);
+                                command.Parameters.AddWithValue("@IsActive", true); // Set as active or based on your logic
+
+                                connection.Open();
+                                command.ExecuteNonQuery();
+                            }
+                        }
+
+                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                        var callbackUrl = Url.Page(
+                            "/Account/ConfirmEmail",
+                            pageHandler: null,
+                            values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                            protocol: Request.Scheme);
+
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                        {
+                            return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    // If adding to role failed, add those errors to the model state
+                    foreach (var error in addToRoleResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /*public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
@@ -130,7 +223,7 @@ namespace OnlineShop.Areas.Identity.Pages.Account
                 
 
 
-                await _userManager.AddToRoleAsync(user, "Regular");
+                await _userManager.AddToRoleAsync(user, "Admin");
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
@@ -169,7 +262,7 @@ namespace OnlineShop.Areas.Identity.Pages.Account
 
             // If we got this far, something failed, redisplay form
             return Page();
-        }
+        } */
 
         private User CreateUser()
         {
