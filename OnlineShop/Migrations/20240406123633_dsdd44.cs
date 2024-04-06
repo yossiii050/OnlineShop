bce7e9a4d8e4b14cb7ac62781e99e0cd275c456b
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2024-04-06 15:46:52 +0300

    fix bug

---
+++ OnlineShop/Migrations/20240406123633_dsdd44.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class dsdd44 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CreditCards",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CreditCards",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
